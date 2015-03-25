e04a4a3908c20826af39df0a230b133e9ce81c5c
sachil <perryspcalpha@gmail.com>
2015-03-25 17:31:37 +0800

    Add transformer animation to the viewPager and add UPNP service.

--- app/src/main/java/com/alphanetworks/uplayer/ui/MainActivity.java
+++ app/src/main/java/com/alphanetworks/uplayer/ui/MainActivity.java
@@ -1,9 +1,13 @@
 package com.alphanetworks.uplayer.ui;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -14,9 +18,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.alphanetworks.uplayer.R;
+import com.alphanetworks.uplayer.Utils;
 import com.alphanetworks.uplayer.ui.adapter.ViewPagerAdapter;
+import com.alphanetworks.uplayer.ui.fragments.DepthPageTransformer;
+import com.alphanetworks.uplayer.upnp.dmr.MediaRenderer;
+import com.alphanetworks.uplayer.upnp.dms.MediaServer;
 
-public class MainActivity extends ActionBarActivity {
+import org.fourthline.cling.android.AndroidUpnpService;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+
+public class MainActivity extends ActionBarActivity implements
+		ServiceConnection {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -25,6 +37,10 @@ public class MainActivity extends ActionBarActivity {
 	private Toolbar mToolbar = null;
 	private ViewPager mViewPager = null;
 	private ViewPagerAdapter mPagerAdapter = null;
+
+	private MediaServer mMediaServer = null;
+	private MediaRenderer mMediaRenderer = null;
+	private AndroidUpnpService mUpnpService = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +57,20 @@ public class MainActivity extends ActionBarActivity {
 
 		mPagerAdapter = new ViewPagerAdapter(mContext,
 				getSupportFragmentManager());
-        mViewPager.setAdapter(mPagerAdapter);
+		mViewPager.setAdapter(mPagerAdapter);
+
+		// 为viewpager设置切换动画
+		mViewPager.setPageTransformer(true, new DepthPageTransformer());
 
 		mNavigation = (DrawerLayout) findViewById(R.id.main_navitagion_layout);
-        TypedValue typedValue = new  TypedValue();
-        mContext.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+		TypedValue typedValue = new TypedValue();
+		mContext.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue,
+				true);
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
 			mNavigation.setStatusBarBackgroundColor(typedValue.data);
 
+		bindService(new Intent(this, AndroidUpnpServiceImpl.class), this,
+				Context.BIND_AUTO_CREATE);
 	}
 
 	@Override
@@ -100,5 +122,25 @@ public class MainActivity extends ActionBarActivity {
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		Log.e(TAG, "Activity configuration changed.");
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName componentName, IBinder service) {
+		Log.e(TAG, "UPNP service is connected.");
+
+		mUpnpService = (AndroidUpnpService) service;
+
+		mMediaServer = new MediaServer(mContext, Utils.getInetAddress(mContext));
+		if (mMediaServer != null)
+			mUpnpService.getRegistry().addDevice(mMediaServer.getDevice());
+		mMediaRenderer = new MediaRenderer(mContext);
+		if (mMediaRenderer != null)
+			mUpnpService.getRegistry().addDevice(mMediaRenderer.getDevice());
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName componentName) {
+		Log.e(TAG, "UPNP service is disconnected.");
+
 	}
 }
